32be801bd87a1ea3dd6a850e915f66f18c1c6b83
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-06-21 13:12:25 +0300

    cleaning spec and script

--- certora/scripts/run.sh
+++ certora/scripts/run.sh
@@ -1,40 +1,8 @@
-# certoraRun certora/harness/AStETHHarness.sol \
-#     certora/harness/SymbolicLendingPool.sol \
-#     certora/harness/DummyERC20A.sol \
-#     certora/harness/DummyERC20B.sol \
-#     --verify AStETHHarness:certora/specs/AStETH.spec \
-#     --link AStETHHarness:POOL=SymbolicLendingPool AStETHHarness:UNDERLYING_ASSET_ADDRESS=DummyERC20A AStETHHarness:RESERVE_TREASURY_ADDRESS=DummyERC20B \
-#     --solc solc6.12 \
-#     --optimistic_loop \
-#     --staging  \
-#     --msg "AStETH rule sanity" \
-#     --rule integrityOfMint
+if [[ "$1" ]]
+then
+    RULE="--rule $1"
+fi
 
-
-
-
-# certoraRun certora/harness/AStETHHarness.sol certora/harness/SymbolicLendingPool.sol certora/harness/DummyERC20A.sol certora/harness/DummyERC20B.sol --verify AStETHHarness:certora/specs/AStETH.spec --link AStETHHarness:POOL=SymbolicLendingPool AStETHHarness:UNDERLYING_ASSET_ADDRESS=DummyERC20A AStETHHarness:RESERVE_TREASURY_ADDRESS=DummyERC20B --solc solc6.12 --optimistic_loop --staging --msg "AStETH rule sanity" --rule integrityOfMint
-
-
-
-
-# certoraRun contracts/protocol/tokenization/lido/AStETH.sol \
-#     certora/harness/SymbolicLendingPool.sol \
-#     certora/harness/DummyERC20A.sol \
-#     certora/harness/DummyERC20B.sol \
-#     --verify AStETH:certora/specs/AStETH.spec \
-#     --link AStETH:POOL=SymbolicLendingPool AStETH:UNDERLYING_ASSET_ADDRESS=DummyERC20A AStETH:RESERVE_TREASURY_ADDRESS=DummyERC20B \
-#     --solc solc6.12 \
-#     --optimistic_loop \
-#     --staging  \
-#     --msg "AStETH rule sanity" \
-#     --rule integrityOfMint
-
-
-# certoraRun contracts/protocol/tokenization/lido/AStETH.sol certora/harness/SymbolicLendingPool.sol certora/harness/DummyERC20A.sol certora/harness/DummyERC20B.sol --verify AStETH:certora/specs/AStETH.spec --link AStETH:POOL=SymbolicLendingPool AStETH:UNDERLYING_ASSET_ADDRESS=DummyERC20A AStETH:RESERVE_TREASURY_ADDRESS=DummyERC20B --solc solc6.12 --optimistic_loop --staging  --msg "AStETH rule sanity" --rule integrityOfMint
-
-
-# RUN THIS
 certoraRun certora/harness/AStETHHarness.sol certora/harness/IncentivesControllerMock.sol certora/harness/SymbolicLendingPool.sol certora/harness/DummyERC20A.sol certora/harness/DummyERC20B.sol \
            --verify AStETHHarness:certora/specs/AStETH.spec \
            --link AStETHHarness:UNDERLYING_ASSET_ADDRESS=DummyERC20A AStETHHarness:POOL=SymbolicLendingPool AStETHHarness:_incentivesController=IncentivesControllerMock AStETHHarness:RESERVE_TREASURY_ADDRESS=DummyERC20B \
@@ -42,5 +10,5 @@ certoraRun certora/harness/AStETHHarness.sol certora/harness/IncentivesControlle
            --optimistic_loop \
            --settings -smt_nonLinearArithmetic=true \
            --cloud \
-           --rule integrityOfMint \
-           --msg "AStETH rule sanity"
+           $RULE \
+           --msg "AStETH $RULE"
